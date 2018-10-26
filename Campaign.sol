a6a550b09eb4e52bb89b5699bd87bc871e189927
pskanade <k.pranav.apk@gmail.com>
2018-10-27 02:36:43 +0530

    Added some modifiers

--- Campaign.sol
+++ Campaign.sol
@@ -4,6 +4,24 @@ contract Campaign {
     address public cManager;
     string public cDescription;
     uint public cBalance;
+    mapping(address => bool) private cDonors;
+
+    // This is to find out if the memory limit of the used datatype
+    // is reaching to the level of overflow.
+    modifier notOverflowing {
+        require((cBalance + msg.value) == address(this).balance, "The datatype used for balance is overflowing. Please use bigger type.");
+        _;
+    }
+
+    modifier onlyDonor {
+        require(cDonors[msg.sender] == true, "Sender is not a Donor.");
+        _;
+    }
+
+    modifier onlyManager {
+        require(cManager == msg.sender, "Sender is not the Manager.");
+        _;
+    }
 
     constructor(string desc) public {
         cManager = msg.sender;
