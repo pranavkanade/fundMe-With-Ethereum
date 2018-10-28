9fc02bee6bfd03c7f5bdcef3a31ca25f09a1353a
pskanade <k.pranav.apk@gmail.com>
2018-10-29 02:25:17 +0530

    test: campaign manager to finalize the spending request.

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -156,4 +156,61 @@ describe('Campaign', () => {
         assert.equal(3, numOfApprovers);
         assert.equal(2, numOfYay);
     });
+
+    it('Lets manager finalize the spending request', async () => {
+        await campaign.methods.donateToCampaign()
+            .send({
+                from: accounts[1],
+                gas: '1000000',
+                value: '200'
+            });
+        await campaign.methods.donateToCampaign()
+            .send({
+                from: accounts[2],
+                gas: '1000000',
+                value: '200'
+            });
+        await campaign.methods.donateToCampaign()
+            .send({
+                from: accounts[3],
+                gas: '1000000',
+                value: '200'
+            });
+
+        // raise a spending request
+        await campaign.methods.raiseSpendingRequest(
+            "Spending request test - 1",
+            '300'
+        ).send({
+            from: accounts[0],
+            gas: '1000000'
+        });
+
+        // vote on the above spending request - [requestId = 0]
+        await campaign.methods.voteOnSpendingRequest('0', '1')
+            .send({
+                from: accounts[1],
+                gas: '1000000'
+            });
+        await campaign.methods.voteOnSpendingRequest('0', '2')
+            .send({
+                from: accounts[2],
+                gas: '1000000'
+            });
+        await campaign.methods.voteOnSpendingRequest('0', '1')
+            .send({
+                from: accounts[3],
+                gas: '1000000'
+            });
+
+        const managersBalBeforeFinalize = await web3.eth.getBalance(accounts[0]);
+        const fundsInCampaignBefore = await web3.eth.getBalance(campaignAddress);
+        await campaign.methods.finalizeSpendingRequest(0).send({
+            from: accounts[0],
+            gas: '1000000'
+        });
+        const fundsInCampaignAfter = await web3.eth.getBalance(campaignAddress);
+
+        assert((fundsInCampaignBefore - fundsInCampaignAfter) == 300);
+    });
 });
