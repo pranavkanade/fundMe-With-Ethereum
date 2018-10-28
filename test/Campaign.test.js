9231edb79240a8bf6af01d2a6848134157cf5f1e
pskanade <k.pranav.apk@gmail.com>
2018-10-29 00:50:40 +0530

    Added basic test case for campaign contract

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -17,15 +17,35 @@ let campaignAddress;
 
 beforeEach(async () => {
     accounts = await web3.eth.getAccounts();
-    console.log(accounts.length);
+    // console.log(accounts.length);
 
     factory = await new web3.eth.Contract(JSON.parse(compiledFactoryCampaign.interface))
         .deploy({ data: compiledFactoryCampaign.bytecode })
         .send({ from: accounts[0], gas: '2000000'}); // had to increase gas limit from 1000000
+
+    await factory.methods.startCampaign("test1", '100')
+        .send({ from: accounts[0], gas: '2000000' });
+    // console.log(campaignAddress);
+
+    [campaignAddress,] = await factory.methods.getDeployedContracts().call();
+    // console.log(campaignAddress);
+
+    // get the contract object of already deployed contract using
+    // an address of that contract.
+    campaign = await new web3.eth.Contract(
+        JSON.parse(compiledCampaign.interface),
+        campaignAddress
+    );
 })
 
 describe('FactoryCampaign', () => {
     it('Gets Deployed', () => {
         assert.ok(factory.options.address);
+    });
+});
+
+describe('Campaign', () => {
+    it('Gets Deployed', () => {
+        assert.ok(campaign.options.address);
     });
 });
