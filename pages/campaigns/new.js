da3fc29032298621d2193d5a54c6294d03b9ed15
pskanade <k.pranav.apk@gmail.com>
2018-10-30 02:18:35 +0530

    Preparing to add onsubmit event handler

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -2,10 +2,39 @@ import React, { Component } from 'react';
 import Layout from '../../components/Layout';
 
 class CampaignNew extends Component {
+    state = {
+        description: '',
+        minContib: ''
+    };
+
+    onSubmit = () => {
+        event.preventDefault(); // keeps browser from submit form
+
+    }
+
     render () {
         return (
             <Layout>
-                <h1>New Campaign</h1>
+                <h3>Create a Campaign</h3>
+                <Form onSubmit={this.onSubmit}>
+                    <Form.Field>
+                    <label>Campaign Description</label>
+                    <Input
+                        placeholder='To save the humanity'
+                        value={this.state.description}
+                        onChange={event => this.setState({ description: event.target.value })}
+                    />
+                    </Form.Field>
+                    <Form.Field>
+                    <label>Minimun Contribution</label>
+                    <Input
+                        label="wei" placeholder='1000' labelPosition="right"
+                        value={this.state.minContib}
+                        onChange={event => this.setState({ minContib: event.target.value})}
+                    />
+                    </Form.Field>
+                    <Button type='submit' primary>Create!</Button>
+                </Form>
             </Layout>
         );
     }
