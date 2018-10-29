181ac5799182f624bf036361fb109b1ae9c6a56c
pskanade <k.pranav.apk@gmail.com>
2018-10-30 00:14:03 +0530

    Corrected the typo

--- contracts/web3.js
+++ contracts/web3.js
@@ -12,7 +12,7 @@ if (typeof window != 'undefined' && typeof window.web3 != 'undefined') {
     web3 = new Web3(window.web3.currentProvider);
 } else {
     // We are on the server *OR* the user is not running metamask
-    const provider = new Web3.provider.HttpProvider(
+    const provider = new Web3.providers.HttpProvider(
         'https://rinkeby.infura.io/v3/ca713bd1eafa48d9a37c3d4ab32262c7'
     );
     web3 = new Web3(provider);
