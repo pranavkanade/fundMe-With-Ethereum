2baf41948e21c414ecd4b9b09f97e2f61b9858ef
pskanade <k.pranav.apk@gmail.com>
2018-11-17 20:49:38 +0530

    Printing the addresses on index page

--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,26 @@ import factoryInstance from "./../contracts/factory";
 
 class CampaignIndex extends Component {
   async componentDidMount() {
+    // this function is to load the data
+    // const campaigns = await factoryInstance.methods
+    //   .getDeployedContracts()
+    //   .call();
+    // console.log(campaigns);
+  }
+
+  static async getInitialProps() {
+    // this method is alternative to `componentDidMount` inside
+    // the next nextjs server. So use it to fetch the data.
+    // Hence we can use serverside rendering.
+    // this function will be called without rendering this component
     const campaigns = await factoryInstance.methods
       .getDeployedContracts()
       .call();
-    console.log(campaigns);
+    return { campaigns };
   }
 
   render() {
-    return <h1>Campaign Index!</h1>;
+    return <div>{this.props.campaigns[0]}</div>;
   }
 }
 
