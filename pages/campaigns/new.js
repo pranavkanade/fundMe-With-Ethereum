1cd8437c5addabb2eed942d9ceb57f1dc26e7bd9
pskanade <k.pranav.apk@gmail.com>
2018-11-17 23:12:12 +0530

    Created layout for new campaign creation

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,11 +1,24 @@
 import React, { Component } from "react";
 import Layout from "../../components/Layouts";
-
+import { Form, Button } from "semantic-ui-react";
 class CampaignNew extends Component {
   render() {
     return (
       <Layout>
-        <h1>This is new show page</h1>
+        <h3>Create new Campaign!</h3>
+        <Form>
+          <Form.Field>
+            <label>Description</label>
+            <input placeholder="Fund me to change the world !" />
+          </Form.Field>
+          <Form.Field>
+            <label>Minimum Allowed Contribution</label>
+            <input placeholder="1 ether" />
+          </Form.Field>
+          <Button type="submit" primary>
+            Create
+          </Button>
+        </Form>
       </Layout>
     );
   }
