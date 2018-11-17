8949558c241a53ebb8f8e3823cb6af618228440e
pskanade <k.pranav.apk@gmail.com>
2018-11-18 01:33:49 +0530

    Setup the routing for the nav bar

--- components/Header.js
+++ components/Header.js
@@ -1,15 +1,22 @@
 import React from "react";
 import { Menu } from "semantic-ui-react";
+import { Link } from "./../routes";
 
 export default () => {
   return (
     <Menu style={{ marginTop: "10px" }}>
       <Menu.Menu>
-        <Menu.Item>Fund Me</Menu.Item>
+        <Link route="/">
+          <a className="item">Fund Me!</a>
+        </Link>
       </Menu.Menu>
       <Menu.Menu position="right">
-        <Menu.Item>Campaigns</Menu.Item>
-        <Menu.Item>+</Menu.Item>
+        <Link route="/">
+          <a className="item">Campaigns</a>
+        </Link>
+        <Link route="/campaigns/new">
+          <a className="item">+</a>
+        </Link>
       </Menu.Menu>
     </Menu>
   );
