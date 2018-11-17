91a2fb3938844d0f746a9e1bf152bde6d5adf76b
pskanade <k.pranav.apk@gmail.com>
2018-11-17 22:58:54 +0530

    new page added for creating new campaign

--- components/Layouts.js
+++ components/Layouts.js
@@ -1,10 +1,17 @@
 import React from "react";
 import Header from "./Header";
 import { Container } from "semantic-ui-react";
+import Head from "next/head";
 
 export default props => {
   return (
     <div>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.0/dist/semantic.min.css"
+        />
+      </Head>
       <Container>
         <Header />
         {props.children}
---
+++ pages/campaigns/new.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Layout from "../../components/Layouts";
+
+class CampaignNew extends Component {
+  render() {
+    return (
+      <Layout>
+        <h1>This is new show page</h1>
+      </Layout>
+    );
+  }
+}
+
+export default CampaignNew;
