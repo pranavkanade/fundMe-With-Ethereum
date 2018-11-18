dea7f1b75bfdf1ac84483b29312bad7a6ba90f7d
pskanade <k.pranav.apk@gmail.com>
2018-11-18 14:58:05 +0530

    routes updated to work for SR pages

--- routes.js
+++ routes.js
@@ -2,5 +2,7 @@ const routes = require("next-routes")();
 
 routes
   .add("/campaigns/new", "/campaigns/new")
-  .add("/campaigns/:address", "/campaigns/show");
+  .add("/campaigns/:address", "/campaigns/show")
+  .add("/campaigns/:address/requests", "/requests/")
+  .add("/campaigns/:address/requests/new", "/requests/new");
 module.exports = routes;
