6e8ac1097fb51401b634123fff1e09a342424537
pskanade <k.pranav.apk@gmail.com>
2018-11-18 20:06:22 +0530

    Routes added for SR pages

--- routes.js
+++ routes.js
@@ -4,5 +4,6 @@ routes
   .add("/campaigns/new", "/campaigns/new")
   .add("/campaigns/:address", "/campaigns/show")
   .add("/campaigns/:address/requests", "/requests/")
-  .add("/campaigns/:address/requests/new", "/requests/new");
+  .add("/campaigns/:address/requests/new", "/requests/new")
+  .add("/campaigns/:address/requests/:reqid", "/requests/show");
 module.exports = routes;
